c3f4422d7770834aef8e8ad1128fbdf3c9462b16
Calvin Wilkinson <kinsondigital@gmail.com>
2020-10-27 16:37:51 -0400

    Initial project setup

---
+++ Test-App/Test-App/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+
+namespace Test_App
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameWinSettings = new GameWindowSettings();
+            var nativeWinSettings = new NativeWindowSettings();
+
+            var window = new GameWindow(gameWinSettings, nativeWinSettings);
+
+            window.Run();
+        }
+    }
+}
